60986ca009d90810499561c6a8554ca208176ff1
pankajkrbansal <pankajbansal.pkb@gmail.com>
2023-02-19 22:12:56 +0530

    Added marketplace component

--- frontend/src/components/Marketplace.js
+++ frontend/src/components/Marketplace.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+// const ethers = require("ethers")
+import NFTMarketplace from "../utils/NFTMarketplace.json";
 
-// let Marketplace = () => {
-//     return(
-//         <></>
-//     );
-// }
 function Marketplace() {
-    return(
-        <></>
-    );
-}
+  const CONTRACT_ADDR = "";
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(CONTRACT_ADDR, NFTMarketplace.abi, signer);
 
+  const [nftData, setNFTData] = useState([]);
+
+  let getAllNFT = async () => {
+    let data = await contract.getAllNFTs();
+    if (data.length > 0) {
+      setNFTData(data);
+    }
+  };
+
+  useEffect(async () => {
+    await getAllNFT();
+  }, nftData);
+
+  if(nftData.length > 0){
+    return (
+        <>
+          <h1>NFT Marketplace</h1>
+        </>
+      );
+  }else{
+    return (
+        <>
+        <h1>No NFT Data To Show Below</h1>
+        </>
+    )
+  }
+}
 
 export default Marketplace;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 let Navbar = () => {
-
+    <>
+        <p>Navbar Component</p>
+    </>
 }
 
 export default Navbar;
